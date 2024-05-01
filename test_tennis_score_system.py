5f2fabf4b7791a9014cc9e3abd3d634f5ab0fc2f
francis billa <francis.billa.fb@gmail.com>
2024-05-01 23:14:45 +0200

    Improve test

--- test_tennis_score_system.py
+++ test_tennis_score_system.py
@@ -1,72 +1,91 @@
+
 import pytest
 from tennis_score_system import TennisScoreCalculator
 
 
 @pytest.fixture
-def game():
+def tennis_calculator():
     return TennisScoreCalculator()
 
 
-def test_regular_score(game):
-    game.point_scored(1)
-    assert game.get_score() == "15-Love"
-    game.point_scored(2)
-    assert game.get_score() == "15-15"
-    game.point_scored(2)
-    assert game.get_score() == "15-30"
-    game.point_scored(1)
-    game.point_scored(1)
-    assert game.get_score() == "40-30"
-    game.point_scored(2)
-    assert game.get_score() == "Deuce 40-40"
+def test_log_score(tennis_calculator):
+    tennis_calculator.log_score(1)
+    assert tennis_calculator.player1_score == 1
+
+    tennis_calculator.log_score(2)
+    assert tennis_calculator.player2_score == 1
 
 
-def test_advantage_score(game):
-    game.point_scored(1)
-    game.point_scored(1)
-    game.point_scored(1)
-    game.point_scored(2)
-    game.point_scored(2)
-    game.point_scored(2)
-    game.point_scored(2)
-    assert game.get_score() == "Advantage Player 2"
-    game.point_scored(1)
-    game.point_scored(1)
-    assert game.get_score() == "Advantage Player 1"
+def test_check_game_won(tennis_calculator):
+    tennis_calculator.player1_score = 4
+    tennis_calculator.player2_score = 2
+    tennis_calculator.check_game_won()
+    assert tennis_calculator.player1_games == 1
+
+    tennis_calculator.player1_score = 3
+    tennis_calculator.player2_score = 5
+    tennis_calculator.check_game_won()
+    assert tennis_calculator.player2_games == 1
 
 
-def test_game_won(game):
-    game.point_scored(1)
-    game.point_scored(1)
-    game.point_scored(1)
-    game.point_scored(1)
-    game.point_scored(1)
-    assert game.player1_games == 1
+def test_get_regular_score(tennis_calculator):
+    tennis_calculator.player1_score = 3
+    tennis_calculator.player2_score = 2
+    assert tennis_calculator.get_regular_score() == "40-30"
 
 
-def test_player1_wins_game(game):
-    game.player1_score = 4
-    game.player2_score = 2
-    game.check_game_won()
-    assert game.player1_games == 1
-    assert game.player2_games == 0
-    assert game.player1_score == 0
-    assert game.player2_score == 0
+def test_get_advantage_score(tennis_calculator):
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(2)
+    tennis_calculator.log_score(2)
+    tennis_calculator.log_score(2)
+    tennis_calculator.log_score(2)
+    assert tennis_calculator.get_score() == "Advantage Player 2"
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(1)
+    assert tennis_calculator.get_score() == "Advantage Player 1"
 
 
-def test_player2_wins_game(game):
-    game.player1_score = 3
-    game.player2_score = 5
-    game.check_game_won()
-    assert game.player1_games == 0
-    assert game.player2_games == 1
-    assert game.player1_score == 0
-    assert game.player2_score == 0
+def test_get_game_score(tennis_calculator):
+    tennis_calculator.player1_score = 4
+    tennis_calculator.player2_score = 2
+    assert tennis_calculator.get_game_score() == "Game Player 1"
 
 
-def test_reset_scores(game):
-    game.player1_score = 4
-    game.player2_score = 2
-    game.check_game_won()
-    assert game.player1_score == 0
-    assert game.player2_score == 0
+def test_game_won(tennis_calculator):
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(1)
+    tennis_calculator.log_score(1)
+    assert tennis_calculator.player1_games == 1
+
+
+def test_player1_wins_game(tennis_calculator):
+    tennis_calculator.player1_score = 4
+    tennis_calculator.player2_score = 2
+    tennis_calculator.check_game_won()
+    assert tennis_calculator.player1_games == 1
+    assert tennis_calculator.player2_games == 0
+    assert tennis_calculator.player1_score == 0
+    assert tennis_calculator.player2_score == 0
+
+
+def test_player2_wins_game(tennis_calculator):
+    tennis_calculator.player1_score = 3
+    tennis_calculator.player2_score = 5
+    tennis_calculator.check_game_won()
+    assert tennis_calculator.player1_games == 0
+    assert tennis_calculator.player2_games == 1
+    assert tennis_calculator.player1_score == 0
+    assert tennis_calculator.player2_score == 0
+
+
+def test_reset_scores(tennis_calculator):
+    tennis_calculator.player1_score = 4
+    tennis_calculator.player2_score = 2
+    tennis_calculator.check_game_won()
+    assert tennis_calculator.player1_score == 0
+    assert tennis_calculator.player2_score == 0
